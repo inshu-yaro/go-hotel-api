bc3dff6052bcb2f6ae9dd4943edd5f7ef3eaa67a
Daniel Perez <tuvistavie@gmail.com>
2014-09-01 11:17:49 +0900

    Add Procfile.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn go_hotel:go_hotel --log-file -
--- go_hotel/__init__.py
+++ go_hotel/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask.ext.cors import CORS
+from werkzeug.contrib.fixers import ProxyFix
 
 go_hotel = Flask(__name__)
 cors = CORS(go_hotel)
 from .config import config
 go_hotel.config.update(**config)
+
+go_hotel.wsgi_app = ProxyFix(go_hotel.wsgi_app)
 
 from flask import Flask
 from sqlalchemy import create_engine
